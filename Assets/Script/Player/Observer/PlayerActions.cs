a17b1ad0fa391aca0af47117a04554f3254bada0
FranciscoPasini <164237310+FranciscoPasini@users.noreply.github.com>
2024-11-11 06:56:54 -0300

    Final Power Ups

--- Assets/Script/Player/Observer/PlayerActions.cs
+++ Assets/Script/Player/Observer/PlayerActions.cs
@@ -22,22 +22,19 @@ public class PlayerActions : MonoBehaviour
 
     private void Update()
     {
-        // Power-up de velocidad activado con la tecla "V"
-        if (Input.GetKeyDown(KeyCode.V) && speedBoostObject.activeSelf)
+        if (Input.GetKeyDown(KeyCode.Q) && speedBoostObject.activeSelf)
         {
             playerManagment.ActivateSpeedBoost();
             speedBoostObject.SetActive(false); // Desactiva el objeto de velocidad
         }
 
-        // Power-up de inmunidad activado con la tecla "R"
-        if (Input.GetKeyDown(KeyCode.R) && immunityObject.activeSelf)
+        if (Input.GetKeyDown(KeyCode.E) && immunityObject.activeSelf)
         {
-            playerHealth.ActivateImmunity(5f); // Inmunidad por 5 segundos
+            playerHealth.ActivateImmunity(3f); // Inmunidad por 3 segundos
             immunityObject.SetActive(false); // Desactiva el objeto de inmunidad
         }
 
-        // Power-up de vida extra activado con la tecla "P"
-        if (Input.GetKeyDown(KeyCode.P) && extraLifeObject.activeSelf)
+        if (Input.GetKeyDown(KeyCode.R) && extraLifeObject.activeSelf)
         {
             playerHealth.ExtraLife();
             extraLifeObject.SetActive(false); // Desactiva el objeto de vida extra
